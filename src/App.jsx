ff3f8486fdc640fe0f759352d5cab4486a05bad1
Nelly-B <alapanelly@gmail.com>
2024-10-27 19:23:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Desserts } from './conponents/Desserts'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Desserts />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/conponents/Desserts.jsx
@@ -0,0 +1,16 @@
+export const Desserts = () =>{
+    return(
+        <>
+        <div className="w-full pl-6 pr-6 h-screen">
+            <div className="place-content-center w-[60%]">
+                <div className="grid grid-cols-3 w-full">
+                    <div className="p-4">col 1</div>
+                    <div className="p-4">col 1</div>
+                    <div className="p-4">col 1</div>
+                </div>
+            </div>
+            <div className="place-content-center w-[20%]"></div>
+        </div>
+        </>
+    )
+}
