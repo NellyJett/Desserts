6791a200eaa95c8f80bfa80c7b4d75b1f109ccd3
Nelly-B <alapanelly@gmail.com>
2024-10-27 22:03:31 +0100

    second commit

--- src/conponents/Desserts.jsx
+++ src/conponents/Desserts.jsx
@@ -1,12 +1,74 @@
 export const Desserts = () =>{
     return(
         <>
-        <div className="w-full pl-6 pr-6 h-screen">
-            <div className="place-content-center w-[60%]">
-                <div className="grid grid-cols-3 w-full">
-                    <div className="p-4">col 1</div>
-                    <div className="p-4">col 1</div>
-                    <div className="p-4">col 1</div>
+        <div className="w-full pl-6 pr-6 h-screen mt-6">
+            <div className="place-content-center sm:w-[60%]">
+                <div className="grid sm:grid-cols-3 grid-cols-1 w-full gap-4 pb-6">
+                    <div className="relative">
+                        <div className="img-1 p-4 min-h-[150px] relative "></div>
+                        <div className="justify-center items-center flex flex-col absolute bottom-[-50px] transform -translate-x-1/2 justify-center items-center left-1/2 items-center ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                            
+                            <div className="">
+                                <p className="mt-2">Waffle</p>
+                            </div>
+                                
+                            
+                        </div>
+                        
+                    </div>
+                    <div className="relative">
+                        <div className="img-2 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <div className="img-3 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid sm:grid-cols-3 grid-cols-1 w-full  gap-4 pb-6">
+                <div className="relative">
+                        <div className="img-4 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <div className="img-5 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <div className="img-6 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid sm:grid-cols-3 grid-cols-1 w-full gap-4 pb-6">
+                <div className="relative">
+                        <div className="img-7 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <div className="img-8 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
+                    <div className="relative">
+                        <div className="img-9 p-4 min-h-[150px] relative ">col 1</div>
+                        <div className="justify-center items-center flex absolute bottom-[-20px] transform -translate-x-1/2 justify-center items-center left-1/2 ">
+                            <button className="rounded-full border border-black-2 p-2 bg-[#ffffff] shadow-xl shadow-lg w-40">Add to Cart</button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="place-content-center w-[20%]"></div>
